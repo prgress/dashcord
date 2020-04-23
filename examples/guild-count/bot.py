05fd002066e9c7798f14cd847eb50bd02161285a
Logan Webb <lganwebb04@gmail.com>
2020-04-23 12:37:20 +0100

    Dashcord BETA

---
+++ examples/guild-count/bot.py
@@ -0,0 +1,30 @@
+import asyncio
+
+# The library imports
+import dashcord
+
+import discord
+from discord.ext import commands
+
+# Import the routing file
+import routes
+
+bot = commands.Bot(command_prefix="!", case_insensitive=True)
+
+# The dashcord.App class takes your bot object, the path to your template folder,
+# the path to your static folder, and the routing file.
+app = dashcord.App(bot, template_path="templates", static_path="static", routing_file=routes)
+
+if __name__ == "__main__":
+    # In order to run both the bot and the web-server within the same asyncio loop,
+    # the bot start is a bit different.
+    
+    loop = asyncio.get_event_loop()
+    
+    loop.create_task(bot.run("TOKEN"))
+    loop.create_task(app.start())
+    
+    loop.run_forever()
+    
+    # dashcord.App.start() will create a variable in your bot called `dashboard`.
+    # It can be accessed using bot.dashboard
---
+++ examples/guild-count/routes.py
@@ -0,0 +1,29 @@
+# This file will contain all of our website's routes
+
+# Route method formating is shown below:
+
+# If your route function is named "index", it's url will be `url/`
+async def index(bot, request):
+    """The function name is what the URL path will be, e.g url/route_name"""
+    # request.method returns either GET or POST
+    if request.method == "GET":
+        # We have a GET request.
+        
+        # As this is an example of a guild counter, we want to pass the bot guilds
+        # to the template formatter.
+        guilds = len(bot.guilds)
+        
+        return bot.dashboard.render_html("index.html", guilds=guilds)
+        # bot.dashboard.render_html will format the html file for you.
+    
+    else:
+        # We have a post request.
+        # If the post request is a form, the request json will return the data within
+        # the form.
+        
+        json = await request.json()
+        
+        channel = bot.get_channel(ID)
+        await channel.send("We just made a POST request with the data: `{}`".format(str(json)))
+        
+        return bot.dashboard.render_html("index.html", guilds=guilds)
