c187d7ffd78aa87069fb67d9d313757a0721db08
Logan Webb <lganwebb04@gmail.com>
2020-04-23 13:56:15 +0100

    fixes

--- examples/guild-count/bot.py
+++ examples/guild-count/bot.py
@@ -11,20 +11,16 @@ import routes
 
 bot = commands.Bot(command_prefix="!", case_insensitive=True)
 
+@bot.event
+async def on_ready():
+    print("Bot ready")
+    # We start the dashboard in the on_ready event, to ensure everything works
+    bot.dashboard.start("localhost", 5000)
+    # dashcord.App.start() takes the host and port for your server. To run it on your IP set the host to "0.0.0.0"
+    
 # The dashcord.App class takes your bot object, the path to your template folder,
 # the path to your static folder, and the routing file.
 app = dashcord.App(bot, template_path="templates", static_path="static", routing_file=routes)
 
 if __name__ == "__main__":
-    # In order to run both the bot and the web-server within the same asyncio loop,
-    # the bot start is a bit different.
-    
-    loop = asyncio.get_event_loop()
-    
-    loop.create_task(bot.run("TOKEN"))
-    loop.create_task(app.start("localhost", 5000))
-    
-    loop.run_forever()
-    
-    # dashcord.App.start() will create a variable in your bot called `dashboard`.
-    # It can be accessed using bot.dashboard
+    bot.run("TOKEN")
